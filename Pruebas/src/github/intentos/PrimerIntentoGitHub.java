2bf5f9706a635135dc3f0da40da0ba6a0e4867fc
Victor Aguado Navarro <aguadonavarrovictor@gmail.com>
2020-05-24 13:44:13 +0200

    Una prueba de subida de un programa en JAVA

---
+++ Pruebas/src/github/intentos/PrimerIntentoGitHub.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package github.intentos;
+
+/**
+ *
+ * @author T440S
+ */
+public class PrimerIntentoGitHub {
+    public static void main(String[] args){
+        System.out.println("GitHub!!!");
+    }
+}
